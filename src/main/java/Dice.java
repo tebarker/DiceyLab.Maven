dc3d867f9c857d75f97b8698c1f566b4be617ce6
Tenae <tenae@zipcodersmbp107.myfiosgateway.com>
2022-07-03 12:48:04 -0400

    dice class

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,35 @@
+
+import java.util.Random;
 public class Dice {
+
+    public int numberOfSides;
+    public int numberOfDice;
+
+    public Dice(int numberOfDice){
+
+        this.numberOfDice = numberOfDice;
+        this.numberOfSides = 6;
+
+    }
+
+    public Dice (int numberOfDice, int numberOfSides){
+        this.numberOfDice = numberOfDice;
+        this.numberOfSides = numberOfSides;
+    }
+
+    public Integer tossAndSum () {
+        Random randomNumber = new Random();
+
+        int sumOfDice = 0;
+
+        for(int i=0; i <= numberOfDice; i++){
+            sumOfDice += (randomNumber.nextInt(6) +1);
+        }
+
+        return sumOfDice;
+
+    }
+
 
 
 }
