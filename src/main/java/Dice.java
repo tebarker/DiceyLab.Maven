e87708c63fbbd77a125218ebb4754b2c16996603
Tenae <tenae@zipcodersmbp107.myfiosgateway.com>
2022-07-03 13:57:46 -0400

    dice tests done :\

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -5,16 +5,24 @@ public class Dice {
     public int numberOfSides;
     public int numberOfDice;
 
-    public Dice(int numberOfDice){
+    public Dice(int numberOfDice){ //my first constructor
 
         this.numberOfDice = numberOfDice;
         this.numberOfSides = 6;
 
     }
 
-    public Dice (int numberOfDice, int numberOfSides){
+    public Dice (int numberOfDice, int numberOfSides){ //my second constructor
         this.numberOfDice = numberOfDice;
         this.numberOfSides = numberOfSides;
+    }
+
+    public int getNumberOfSides() {
+        return numberOfSides;
+    }
+
+    public int getNumberOfDice(){
+        return numberOfDice;
     }
 
     public Integer tossAndSum () {
@@ -23,7 +31,8 @@ public class Dice {
         int sumOfDice = 0;
 
         for(int i=0; i <= numberOfDice; i++){
-            sumOfDice += (randomNumber.nextInt(6) +1);
+            sumOfDice += randomNumber.nextInt(7) ; //nextint finds a random number from 0 (inclusive) to number passed N (exclusive) so bound = 6.
+
         }
 
         return sumOfDice;
